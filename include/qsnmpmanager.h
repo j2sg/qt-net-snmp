0c092bd853eb0b4b48d3e11c515f34f25f8e3db4
Juan José Salazar García <jjslzgc@gmail.com>
2014-08-31 23:49:05 +0200

    Added Doxygen documentation support and the skeleton of QSNMPManager class

---
+++ include/qsnmpmanager.h
@@ -0,0 +1,26 @@
+/**
+ *  This file is part of Qt-Net-SNMP.
+ *
+ *  Copyright (c) 2014 Juan Jose Salazar Garcia jjslzgc@gmail.com
+ *
+ *  QMB is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  QMB is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with QMB.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
+
+#ifndef QSNMPMANAGER_H
+#define QSNMPMANAGER_H
+
+class QSNMPManager;
+
+#endif // QSNMPMANAGER_H
